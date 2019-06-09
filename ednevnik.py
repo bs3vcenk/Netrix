720d3c6d3b2d9e369e9b1e63edee64fb183fa8b8
btx3 <bs3vcenk@gmail.com>
2019-06-09 16:15:16 +0200

    Comment demo script, implement -nohidepriv, implement concluded grades
    The script now has comments (a bit overdue, and a bit too vague).
    
    The getConcludedGradeForSubject() function has been included in the
    avgonly calculation, and grades got through it will be marked
    accordingly.
    
    The -nohidepriv option has been added to the script to control the
    hidepriv argument added to EDAP.

--- ednevnik.py
+++ ednevnik.py
@@ -6,45 +6,58 @@ from getpass import getpass
 sver = "0.3"
 
 def help():
-	print("-debug\t\t\tEnable debug in EDAP module\n-alltests\t\tShow all tests instead of just from current date\n-loglevel 0/1/2/3\tSet log level (verbose/info/warning/error)\n-noanonrep\t\tDon't send anonymous error reports\n-fullavg\t\tShow only average of all classes\n")
+	print("-debug\t\t\tEnable debug in EDAP module\n-alltests\t\tShow all tests instead of just from current date\n-loglevel 0/1/2/3\tSet log level (verbose/info/warning/error)\n-noanonrep\t\tDon't send anonymous error reports\n-fullavg\t\tShow only average of all classes\n-nohidepriv\t\tDon't hide private info in debug logs\n")
 	sys.exit(0)
 
 print("eDnevnikAndroid %s\n" % sver)
+
+## ARG CHECKING
 debug = "-debug" in sys.argv
 alltests = "-alltests" in sys.argv
 loglevel = int(sys.argv[sys.argv.index("-loglevel")+1]) if "-loglevel" in sys.argv else 1
 avgonly = "-fullavg" in sys.argv
-help() if "-h" in sys.argv or "-help" in sys.argv else None
+hidepriv = not "-nohidepriv" in sys.argv
+help() if "-h" in sys.argv or "-help" in sys.argv else None # If -help
+
+# Ask for creds
 print("Please enter your credentials.\nDISCLAIMER: Your credentials are sent only to \"ocjene.skole.hr\"\nand are kept in memory only for the duration of the login process.\n")
 user = input("Username: ")
 pasw = getpass("Password: ")
-dnevnik = edap.edap(user=user, pasw=pasw, debug=debug, loglevel=loglevel)
-del pasw
-del user
+# Init the module
+dnevnik = edap.edap(user=user, pasw=pasw, debug=debug, loglevel=loglevel, hidepriv=hidepriv)
+# Get list of all classes user has on profile
 classes = dnevnik.getClasses()
 cnum = 0
+# Print out class selection, store it to sel variable
 for x in classes:
 	print("ID %s: class: %s, year: %s, school: %s, city: %s, classmaster: %s" % (cnum, x["class"], x["year"], x["school_name"], x["school_city"], x["classmaster"]))
 	cnum += 1
 sel = int(input("\nSelection (0 to %s): " % (cnum-1)))
+# If user requested only average
 if avgonly:
 	subs = dnevnik.getSubjects(sel)
 	fullgr = []
 	for x in range(len(subs)):
-		ocj = []
-		for y in dnevnik.getGradesForSubject(sel, x):
-			try:
-				ocj.append(int(y[2]))
-			except:
-				pass
-		if len(ocj) == 0:
-			continue
+		hasConcludedGrade, concludedGrade = dnevnik.getConcludedGradeForSubject(sel, x)
+		if hasConcludedGrade:
+			print("%s: %s (CONCLUDED)" % (subs[x]["subject"], concludedGrade))
+			fullgr.append(concludedGrade)
 		else:
-			z = round(sum(ocj)/len(ocj), 0)
-			print("%s: %s" % (subs[x]["subject"], z))
-			fullgr.append(z)
+			ocj = []
+			for y in dnevnik.getGradesForSubject(sel, x):
+				try:
+					ocj.append(int(y[2]))
+				except:
+					pass
+			if len(ocj) == 0:
+				continue
+			else:
+				z = int(round(sum(ocj)/len(ocj), 0))
+				print("%s: %s" % (subs[x]["subject"], z))
+				fullgr.append(z)
 	print("AVERAGE: %s" % (round(sum(fullgr)/len(fullgr), 2)) )
 	exit()
+# Else ask for subject selection
 print("\nSubjects:")
 subs = dnevnik.getSubjects(sel)
 for x in range(len(subs)):
@@ -57,12 +70,14 @@ sel_2 = int(input("\nSelection (0 to %s): " % (len(subs)-1)))
 dates = []
 ocjene = []
 status = []
+# Print out grades for selected subject (including reason/subject and date)
 for x in dnevnik.getGradesForSubject(sel,sel_2):
 	dates.append(x[0])
 	ocjene.append(int(x[2]))
 	status.append(x[1])
 print("")
 print(tabulate({"Date":dates, "Subject":status, "Grade":ocjene}, headers="keys"))
+# Calculate avg for subject, round to 2 decimals and 0 decimals
 avg1 = round(sum(ocjene)/len(ocjene), 2)
 avg2 = round(avg1, 0)
 print("\nAVERAGE FOR SUBJECT ID %s: %s (%s)" % (sel_2, str(avg2)[:1], avg1))
@@ -70,6 +85,7 @@ print("\nTests:\n")
 subjnames = []
 testsubjs = []
 testdates = []
+# Print out remaining or all tests
 for x in dnevnik.getTests(sel, alltests=alltests):
 	subjnames.append(x[0])
 	testsubjs.append(x[1])
