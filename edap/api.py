28efe7c1a5e47658285bdc86647a59889b92b90e
btx3 <bs3vcenk@gmail.com>
2019-06-22 14:30:23 +0200

    Temporarily return populateData(), fix del statement

--- edap/api.py
+++ edap/api.py
@@ -2,6 +2,7 @@ import edap, platform, threading
 from flask import Flask, jsonify, make_response, request, abort
 from flask import __version__ as _flaskVersion
 from hashlib import md5 as _MD5HASH
+from copy import deepcopy
 
 api_version = "1.0-dev"
 
@@ -25,39 +26,38 @@ class DataPopulationThread(threading.Thread):
 		self.status = None
 		super().__init__()
 
-	def populateData(obj):
-		"""
-			Fill in the 'data' part of the user dict. This will contain subjects, grades, etc.
-		"""
-		dataDict = {'classes':None, 'tests':None}
+def populateData(obj):
+	"""
+		Fill in the 'data' part of the user dict. This will contain subjects, grades, etc.
+	"""
+	dataDict = {'classes':None, 'tests':None}
+	try:
+		#self.status = {"status":"S_CLASSES","progress":None}
+		cl = obj.getClasses()
+	except Exception as e:
+		print('PDATA || Error getting classes: %s' % e)
+		abort(500)
 
+	output = cl
+	for x in range(len(output)):
+		#self.status = {"status":"S_SUBJECTS","progress":"%s/%s" % (x, len(output))}
 		try:
-			self.status = {"status":"S_CLASSES","progress":None}
-			cl = obj.getClasses()
+			output[x]['subjects'] = obj.getSubjects(x)
 		except Exception as e:
-			print('PDATA || Error getting classes: %s' % e)
-			abort(500)
-
-		output = cl
-		for x in range(len(output)):
-			self.status = {"status":"S_SUBJECTS","progress":"%s/%s" % (x, len(output))}
+			print('PDATA || Error getting subjects for class %s: %s' % (x, e))
+			output[x]['subjects'] = None
+			continue
+		for z in range(len(output[x]['subjects'])):
+			#self.status = {"status":"S_GRADES", "progress":"%s/%s" % (z+1, len(output[x]['subjects'])+1)}
 			try:
-				output[x]['subjects'] = obj.getSubjects(x)
+				output[x]['subjects'][z]['grades'] = obj.getGradesForSubject(x, z)
 			except Exception as e:
-				print('PDATA || Error getting subjects for class %s: %s' % (x, e))
-				output[x]['subjects'] = None
+				print('PDATA || Error getting grades for class %s, subject %s: %s' % (x, z, e))
+				output[x]['subjects'][z]['grades'] = None
 				continue
-			for z in range(len(output[x]['subjects'])):
-				self.status = {"status":"S_GRADES", "progress":"%s/%s" % (z+1, len(output[x]['subjects'])+1)}
-				try:
-					output[x]['subjects'][z]['grades'] = obj.getGradesForSubject(x, z)
-				except Exception as e:
-					print('PDATA || Error getting grades for class %s, subject %s: %s' % (x, z, e))
-					output[x]['subjects'][z]['grades'] = None
-					continue
-		self.status = {'status':'S_DONE', 'progress':None}
-		dataDict['classes'] = output
-		return dataDict
+	#self.status = {'status':'S_DONE', 'progress':None}
+	dataDict['classes'] = output
+	return dataDict
 
 @app.errorhandler(404)
 def e404(err):
@@ -109,7 +109,7 @@ def getClasses(token):
 	if token not in users.keys():
 		print('CLASS || Token %s does not exist' % token)
 		abort(401)
-	o = users[token]['data']
+	o = deepcopy(users[token]['data'])
 	for i in o['classes']:
 		del i['subjects']
 	return make_response(jsonify(o), 200)
