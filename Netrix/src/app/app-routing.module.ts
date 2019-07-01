96cf3df332b45663462350a0a3834ca44d4e5004
btx3 <bs3vcenk@gmail.com>
2019-07-01 18:01:52 +0200

    Remove data page

--- Netrix/src/app/app-routing.module.ts
+++ Netrix/src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ const routes: Routes = [
   { path: 'subj-overview/:subjid', loadChildren: './subj-overview/subj-overview.module#SubjOverviewPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'settings/lang', loadChildren: './settings/lang/lang.module#LangPageModule' },
-  { path: 'settings/api', loadChildren: './settings/api/api.module#ApiPageModule' },
-  { path: 'settings/api/data', loadChildren: './settings/api/data/data.module#DataPageModule' }
+  { path: 'settings/api', loadChildren: './settings/api/api.module#ApiPageModule' }
 ];
 @NgModule({
   imports: [
