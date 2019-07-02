ebdd891a2fafc137e49ae7efb04a4563eb75e4a0
btx3 <bs3vcenk@gmail.com>
2019-07-02 15:55:48 +0200

    Fix selected language

--- Netrix/src/app/language.service.ts
+++ Netrix/src/app/language.service.ts
@@ -21,6 +21,8 @@ export class LanguageService {
       if (val) {
         this.setLanguage(val);
         this.selected = val;
+      } else {
+        this.selected = language;
       }
     });
   }
