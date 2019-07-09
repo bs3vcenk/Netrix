1858e75eda2fed67e85f8c73180daaabf1cb125e
btx3 <bs3vcenk@gmail.com>
2019-07-09 16:24:59 +0200

    Implement search with fade in/out animation

--- Netrix/src/app/tab2/tab2.page.ts
+++ Netrix/src/app/tab2/tab2.page.ts
@@ -3,17 +3,40 @@ import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from '../authentication.service';
 import { timeout } from 'rxjs/operators';
 import { SettingsService } from '../settings.service';
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: '0' })),
+      state('*', style({ opacity: '1' })),
+      transition('void <=> *', animate('150ms ease-in'))
+    ])
+  ]
 })
 export class Tab2Page {
 
   tests = null;
   showAllPreference = false;
   currentTests = 0;
+  results = null;
+
+  constructor(
+    private authServ: AuthenticationService,
+    private http: HttpClient,
+    private settings: SettingsService
+  ) {
+    this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/classes/0/tests').pipe(timeout(3000)).subscribe((response) => {
+      this.tests = response.tests;
+      this.results = response.tests;
+      this.countTests();
+    }, (error) => {
+      console.log(error);
+    })
+  }
 
   async countTests() {
     this.tests.forEach((test) => {
@@ -23,20 +46,20 @@ export class Tab2Page {
     });
   }
 
-  constructor(
-    private authServ: AuthenticationService,
-    private http: HttpClient,
-    private settings: SettingsService
-  ) {
-    this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/classes/0/tests').pipe(timeout(3000)).subscribe((response) => {
-      this.tests = response.tests;
-      this.countTests();
-    }, (error) => {
-      console.log(error);
-    })
+  private reInit() {
+    this.results = this.tests;
   }
 
-  ionViewDidLoad() {
+  searchHandler(event) {
+    const val = event.target.value;
+    //console.log(val);
+    if (val && val.trim() != '') {
+      this.results = this.results.filter((item) => {
+        return ((item.test.toLowerCase().indexOf(val.toLowerCase()) > -1) || (item.subject.toLowerCase().indexOf(val.toLowerCase()) > -1));
+      });
+    } else {
+      this.results = this.tests;
+    }
   }
 
 }
