24c5ad0ed37f840d8f39bf441f5a836ab9b0feed
btx3 <bs3vcenk@gmail.com>
2019-07-18 14:06:52 +0200

    Fix types for chart

--- Netrix/src/app/tab4/tab4.page.ts
+++ Netrix/src/app/tab4/tab4.page.ts
@@ -6,7 +6,7 @@ import { ToastController } from '@ionic/angular';
 import { trigger, state, style, animate, transition } from "@angular/animations";
 import { timeout } from 'rxjs/operators';
 import { SettingsService } from '../settings.service';
-import { Chart } from 'chart.js';
+import { Chart, ChartConfiguration, ChartElementsOptions } from 'chart.js';
 
 @Component({
   selector: 'app-tab4',
@@ -50,44 +50,44 @@ export class Tab4Page {
 
   initGraph() {
     Chart.pluginService.register({
-      beforeDraw: (chart) => {
-        if (chart.config.options.elements.center) {
-          //Get ctx from string
-          let ctx = chart.chart.ctx;
+      beforeDraw: (_chart) => {
+        let chart: any = _chart;
+        //Get ctx from string
+        let ctx = chart.chart.ctx;
 
-          //Get options from the center object in options
-          let centerConfig = chart.config.options.elements.center;
-          let fontStyle = centerConfig.fontStyle || 'Arial';
-          let txt = centerConfig.text;
-          let color = centerConfig.color || '#000';
-          let sidePadding = centerConfig.sidePadding || 20;
-          let sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
-          //Start with a base font of 30px
-          ctx.font = "30px " + fontStyle;
+        //Get options from the center object in options
+        let elements: any = chart.config.options.elements;
+        let centerConfig = elements.center;
+        let fontStyle = centerConfig.fontStyle || 'Arial';
+        let txt = centerConfig.text;
+        let color = centerConfig.color || '#000';
+        let sidePadding = centerConfig.sidePadding || 20;
+        let sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+        //Start with a base font of 30px
+        ctx.font = "30px " + fontStyle;
 
-          //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-          let stringWidth = ctx.measureText(txt).width;
-          let elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+        //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        let stringWidth = ctx.measureText(txt).width;
+        let elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
 
-          // Find out how much the font can grow in width.
-          let widthRatio = elementWidth / stringWidth;
-          let newFontSize = Math.floor(30 * widthRatio);
-          let elementHeight = (chart.innerRadius * 2);
+        // Find out how much the font can grow in width.
+        let widthRatio = elementWidth / stringWidth;
+        let newFontSize = Math.floor(30 * widthRatio);
+        let elementHeight = (chart.innerRadius * 2);
 
-          // Pick a new font size so it will not be larger than the height of label.
-          let fontSizeToUse = Math.min(newFontSize, elementHeight);
+        // Pick a new font size so it will not be larger than the height of label.
+        let fontSizeToUse = Math.min(newFontSize, elementHeight);
 
-          //Set font settings to draw it correctly.
-          ctx.textAlign = 'center';
-          ctx.textBaseline = 'middle';
-          let centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-          let centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
-          ctx.font = fontSizeToUse+"px " + fontStyle;
-          ctx.fillStyle = color;
+        //Set font settings to draw it correctly.
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        let centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        let centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+        ctx.font = fontSizeToUse+"px " + fontStyle;
+        ctx.fillStyle = color;
 
-          //Draw text in center
-          ctx.fillText(txt, centerX, centerY);
-        }
+        //Draw text in center
+        ctx.fillText(txt, centerX, centerY);
       }
     });
     this.absenceChart = new Chart(this.absenceCanvas.nativeElement, {
@@ -113,8 +113,8 @@ export class Tab4Page {
           sidePadding: 20 // Defualt is 20 (as a percentage)
         }
       }
-    }
-    });
+    } as ChartConfiguration
+  });
   }
 
   collectStudentData() {
