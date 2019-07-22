7eed735f62a50276a29f87b04ea661f4ca73b162
btx3 <bs3vcenk@gmail.com>
2019-07-22 16:51:04 +0200

    Netrix: Implement notif blacklist, detailed traces

--- Netrix/src/app/api.service.ts
+++ Netrix/src/app/api.service.ts
@@ -11,6 +11,8 @@ import { Firebase } from '@ionic-native/firebase/ngx';
 })
 export class ApiService {
 
+  ignoredNotifTypes = [];
+
   tests = null;
   currentTests = 0;
 
@@ -34,18 +36,60 @@ export class ApiService {
   ) { }
 
   async preCacheData() {
-    this.firebase.startTrace('dataFetch');
     this.getSubjects();
     this.getTests();
     this.getAbsences();
-    this.firebase.stopTrace('dataFetch');
+    this.getNotifConfig();
+  }
+
+  receiveNotifType(nType: string) {
+    if (this.ignoredNotifTypes.includes(nType)) {
+      this.firebase.startTrace('receiveNotifType');
+      this.http.post<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/settings/notif.ignore.del', {parameter: nType})
+      .pipe(timeout(this.settings.httpLimit))
+      .subscribe((response) => {
+        this.firebase.stopTrace('receiveNotifType');
+        delete this.ignoredNotifTypes[this.ignoredNotifTypes.indexOf(nType)];
+      }, (err) => {
+        this.firebase.stopTrace('receiveNotifType');
+        throw err;
+      });
+    }
+  }
+
+  ignoreNotifType(nType: string) {
+    if (!this.ignoredNotifTypes.includes(nType)) {
+      this.firebase.startTrace('ignoreNotifType');
+      this.http.post<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/settings/notif.ignore.add', {parameter: nType})
+      .pipe(timeout(this.settings.httpLimit))
+      .subscribe((response) => {
+        this.firebase.stopTrace('ignoreNotifType');
+        this.ignoredNotifTypes.push(nType);
+      }, (err) => {
+        this.firebase.stopTrace('ignoreNotifType');
+        throw err;
+      });
+    }
+  }
+
+  getNotifConfig() {
+    this.firebase.startTrace('getNotifConfig');
+    this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/settings/notif.all')
+    .pipe(timeout(this.settings.httpLimit))
+    .subscribe((response) => {
+      this.firebase.stopTrace('getNotifConfig');
+      this.ignoredNotifTypes = response.value.ignore;
+    }, (err) => {
+      this.firebase.stopTrace('getNotifConfig');
+      throw err;
+    });
   }
 
   getSubjects() {
+    this.firebase.startTrace('getSubjects');
     this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/classes/0/subjects')
     .pipe(timeout(this.settings.httpLimit))
     .subscribe((response) => {
-      this.subj_attemptedLoad = true;
       const allsubs = response.subjects;
       // Iterate over professors list and join it into a comma-separated string
       allsubs.forEach((subj) => {
@@ -65,10 +109,11 @@ export class ApiService {
       this.fullAvg = response.class_avg;
       this.subj_noItemsLoaded = false;
       this.dbError = false;
+      this.firebase.stopTrace('getSubjects');
     },
     (error) => {
-      this.subj_attemptedLoad = true;
       this.subj_noItemsLoaded = true;
+      this.firebase.stopTrace('getSubjects');
       if (error.error) {
         console.log(error);
         if (error.error.error === 'E_TOKEN_NONEXISTENT') {
@@ -87,6 +132,7 @@ export class ApiService {
   }
 
   getTests() {
+    this.firebase.startTrace('getTests');
     this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/classes/0/tests')
     .pipe(timeout(this.settings.httpLimit))
     .subscribe((response) => {
@@ -94,8 +140,10 @@ export class ApiService {
       this.countTests();
       this.tests_noItemsLoaded = false;
       this.dbError = false;
+      this.firebase.stopTrace('getTests');
     }, (error) => {
       this.tests_noItemsLoaded = true;
+      this.firebase.stopTrace('getTests');
       if (error.error) {
         console.log(error);
         if (error.error.error === 'E_TOKEN_NONEXISTENT') {
@@ -122,11 +170,14 @@ export class ApiService {
   }
 
   getAbsences() {
+    this.firebase.startTrace('getAbsences');
     this.http.get<any>(this.settings.apiServer + '/api/user/' + this.authServ.token + '/classes/0/absences')
     .pipe(timeout(this.settings.httpLimit))
     .subscribe((response) => {
       this.absences = response;
+      this.firebase.stopTrace('getAbsences');
     }, (error) => {
+      this.firebase.stopTrace('getAbsences');
       this.abs_noItemsLoaded = true;
       if (error.error) {
         if (error.error.error === 'E_TOKEN_NONEXISTENT') {
