1c3e32c96a1595af75dc93a4286d3f30f9af6e77
btx3 <bs3vcenk@gmail.com>
2019-07-24 17:29:51 +0200

    Netrix: Disable storing language
    No need to store the language anymore as the default system language is
    used by default.

--- Netrix/src/app/language.service.ts
+++ Netrix/src/app/language.service.ts
@@ -1,34 +1,21 @@
 import { TranslateService } from '@ngx-translate/core';
 import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage';
 import { SettingsService } from './settings.service';
-
-const LNG_KEY = 'SELECTED_LANGUAGE';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
-  selected = '';
 
   constructor(
     private translate: TranslateService,
-    private storage: Storage,
     private settings: SettingsService
   ) { }
 
   setInitialLang() {
     const language = this.translate.getBrowserLang();
     this.translate.setDefaultLang(language);
-
-    this.storage.get(LNG_KEY).then(val => {
-      if (val) {
-        this.setLanguage(val);
-        this.settings.language = val;
-      } else {
-        this.settings.language = language;
-      }
-    });
+    this.setLanguage(language);
   }
 
   getLanguages() {
@@ -42,6 +29,5 @@ export class LanguageService {
   setLanguage(lng) {
     this.translate.use(lng);
     this.settings.language = lng;
-    this.storage.set(LNG_KEY, lng);
   }
 }
