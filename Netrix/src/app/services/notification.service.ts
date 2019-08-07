c77f6708ea42e26f7212cb3b591fddfd3523d316
btx3 <bs3vcenk@gmail.com>
2019-08-07 23:04:30 +0200

    Netrix: Write parts of NotificationService
    Not tested

--- Netrix/src/app/services/notification.service.ts
+++ Netrix/src/app/services/notification.service.ts
@@ -1,20 +1,81 @@
 import { Injectable } from '@angular/core';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { LocalNotifications, ILocalNotification } from '@ionic-native/local-notifications/ngx';
+import { ApiService } from './api.service';
+import { BehaviorSubject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor(
-    private notif: LocalNotifications
-  ) { }
+  notifInitFinished = new BehaviorSubject(false);
+  scheduledNotifs = [];
+  scheduledNotifIDs = []
+  oneDayInMiliseconds = 86400000;
 
-  testNotif() {
-    const a = this.notif.schedule({
-      text: 'Hello',
-      trigger: {at: new Date(new Date().getTime() + 10)},
+  constructor(
+    private notif: LocalNotifications,
+    private apiSvc: ApiService,
+    private translate: TranslateService
+  ) {
+    /* Get all scheduled notifications and let scheduleTestNotifications
+     * know we're done */
+    this.syncLocalLists();
+  }
+
+  syncLocalLists() {
+    this.notif.getAll().then(notifs => {
+      notifs.forEach(notifX => {
+        this.scheduledNotifIDs.push(notifX.id);
+      });
+      this.scheduledNotifs = notifs;
+      this.notifInitFinished.next(true);
     });
-    console.log(this.notif.getAll());
+  }
+
+  disableNotif(id: number) {
+    /* Disable notification for a test */
+    if (this.scheduledNotifIDs.includes(id)) {
+      this.notif.cancel(id);
+      this.syncLocalLists();
+    }
+  }
+
+  disableAll() {
+    this.notif.cancelAll();
+    this.syncLocalLists();
+  }
+
+  scheduleTestNotifications(days: number) {
+    /* Wait until notif.getAll() is done */
+    this.notifInitFinished.subscribe(val => {
+      if (val) {
+        console.log('NotificationService/scheduleTestNotifications(): notifInitFinished is true');
+        // tslint:disable-next-line: prefer-const
+        let toBeScheduled = [];
+        /* Format every 'current' test into a notification and append it
+         * to a list of to-be-scheduled tests */
+        this.apiSvc.currentTests.forEach(test => {
+          /* If it's already scheduled, avoid scheduling again */
+          if (!this.scheduledNotifIDs.includes(test.id)) {
+            toBeScheduled.push({
+              id: test.id,
+              title: this.translate.instant('notif.text.test'),
+              text: test.subject + ':' + test.test,
+              foreground: true,
+              trigger: { at: new Date((test.date * 1000) - (days * this.oneDayInMiliseconds)) }
+            } as ILocalNotification);
+            this.apiSvc.tests[test.id].scheduled = true;
+          }
+        });
+        this.scheduleNotifications(toBeScheduled);
+      }
+    });
+  }
+
+  private scheduleNotifications(notifications: ILocalNotification | ILocalNotification[]) {
+    /* Schedules a list of notifications. Time is in miliseconds */
+    this.notif.schedule(notifications);
   }
 }
