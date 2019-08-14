ebd70902a575038a59e5d02557f7eb023453ac10
btx3 <bs3vcenk@gmail.com>
2019-08-14 14:20:54 +0200

    eDAP: Swap default parser for lxml

--- edap/app/edap.py
+++ edap/app/edap.py
@@ -39,7 +39,7 @@ class edap:
 	def __init__(self,
 	             user: str,
 	             pasw: str,
-	             parser="html.parser",
+	             parser="lxml",
 	             edurl="https://ocjene.skole.hr",
 	             ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0",
 	             debug=False,
