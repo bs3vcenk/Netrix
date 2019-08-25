5aa087792dbcda51b34a5b2c9151561c7ad4622a
btx3 <bs3vcenk@gmail.com>
2019-08-25 13:35:56 +0200

    eDAP: Add populate_data/api testing to tests

--- edap/app/tests.py
+++ edap/app/tests.py
@@ -7,6 +7,122 @@ def to_ms(seconds: int) -> str:
 	"""Convert seconds to miliseconds"""
 	return str(round(seconds * 1000)) + ' ms'
 
+def populate_data(obj):
+	"""
+		Fill in the 'data' part of the user dict. This will contain subjects, grades, etc.
+
+		First, get the class list (this also fills the class ID list for eDAP).
+
+		Second, get the list of tests for the first class, both current and full, and
+		compare them, assigning a "current" flag to each which will say if the test
+		has already been written or not.
+
+		Third, get the subjects for a class, and get the grades for each one. If there
+		is a concluded grade available, use it as the average, otherwise calculate an average.
+		Get the list of "additional notes" for each subject.
+
+		Fourth, write this data into the "classes" key in the dict.
+
+		Fifth, get the user's personal info and write it into the "info" key in the dict.
+
+		Finally, return all the collected data.
+	"""
+	start_full = timer()
+	dataDict = {'classes':None, 'info':None}
+	try:
+		start = timer()
+		output = obj.getClasses()
+		end = timer()
+		print('==> getClasses() => %s' % to_ms(end - start))
+	except Exception as e:
+		print("Error getting classes: %s" % e)
+		sys.exit(1)
+
+	try:
+		start_fetch = timer()
+		tests_nowonly = obj.getTests(0, alltests=False)
+		tests_all = obj.getTests(0, alltests=True)
+		end_fetch = timer()
+		start_proc = timer()
+		testId = 0
+		for x in tests_all:
+			if x not in tests_nowonly:
+				x['current'] = False
+			else:
+				x['current'] = True
+			x['id'] = testId
+			testId += 1
+		output[0]['tests'] = tests_all
+		end_proc = timer()
+		print('==> getTests() [fetch only] => %s' % to_ms(end_fetch - start_fetch))
+		print('==> getTests() [processing] => %s' % to_ms(end_proc - start_proc))
+	except Exception as e:
+		print("Error getting tests for class: %s" % e)
+		output[0]['tests'] = None
+
+	try:
+		start = timer()
+		absences_overview = obj.getAbsentOverviewForClass(0)
+		absences_full = obj.getAbsentFullListForClass(0)
+		output[0]['absences'] = {'overview':absences_overview, 'full':absences_full}
+		end = timer()
+		print('==> getAbsent*ForClass() => %s' % to_ms(end - start))
+	except Exception as e:
+		print("Error getting absences for class: %s" % e)
+		output[0]['absences'] = None
+
+	try:
+		start = timer()
+		output[0]['subjects'] = obj.getSubjects(0)
+		end = timer()
+		print('==> getSubjects() => %s' % to_ms(end - start))
+	except Exception as e:
+		print("Error getting subjects for class: %s" % e)
+		output[0]['subjects'] = None
+	start = timer()
+	allSubjAverageGrades = []
+	for z in range(len(output[0]['subjects'])):
+		output[0]['subjects'][z]['id'] = z
+		try:
+			output[0]['subjects'][z]['grades'] = obj.getGradesForSubject(0, z)
+			if len(output[0]['subjects'][z]['grades']) == 0:
+				output[0]['subjects'][z]['grades'] = None
+			isconcl, concluded = obj.getConcludedGradeForSubject(0, z)
+			if isconcl:
+				output[0]['subjects'][z]['average'] = concluded
+				allSubjAverageGrades.append(concluded)
+			else:
+				lgrades = []
+				for i in output[0]['subjects'][z]['grades']:
+					lgrades.append(i['grade'])
+				output[0]['subjects'][z]['average'] = round(sum(lgrades)/len(lgrades), 2)
+				allSubjAverageGrades.append(round(sum(lgrades)/len(lgrades), 0))
+		except Exception as e:
+			print("Error getting grades for subject %s: %s" % (z, e))
+			output[0]['subjects'][z]['grades'] = None
+			continue
+		try:
+			output[0]['subjects'][z]['notes'] = obj.getNotesForSubject(0, z)
+			if len(output[0]['subjects'][z]['notes']) == 0:
+				output[0]['subjects'][z]['notes'] = None
+		except Exception as e:
+			print("Error getting notes for subject %s: %s" % (z, e))
+			output[0]['subjects'][z]['notes'] = None
+			continue
+	output[0]['complete_avg'] = round(sum(allSubjAverageGrades)/len(allSubjAverageGrades), 2)
+	end = timer()
+	print('==> grade, note & average fetch and processing => %s' % to_ms(end - start))
+	dataDict['classes'] = output
+	try:
+		start = timer()
+		dataDict['info'] = obj.getInfoForUser(0)
+		end = timer()
+		print('==> getInfoForUser() => %s' % to_ms(end - start))
+	except Exception as e:
+		print("Error getting info: %s" % (str(e)))
+	end_full = timer()
+	print('=======> populateData() simulation took: %s' % (to_ms(end_full - start_full)))
+
 def main():
 	"""Main function"""
 	full_start = timer()
@@ -136,4 +252,13 @@ if __name__ == "__main__":
 	elif "--parser" in ARGS:
 		PARSER = sys.argv[sys.argv.index('--parser')+1]
 		print('= conf => Using %s as parser' % PARSER)
-	main()
+	if "--type" in ARGS:
+		TYPE = sys.argv[sys.argv.index('--type')+1]
+		print('= conf => Testing with %s method' % TYPE)
+	else:
+		TYPE = 'bare'
+		print('= conf => Testing bare eDAP library')
+	if TYPE == 'bare':
+		main()
+	elif TYPE == 'api':
+		populate_data(edap.edap(sys.argv[1], sys.argv[2]))
