1ed815ca90e5083b366508b9d90cfacb9e593cd7
btx3 <bs3vcenk@gmail.com>
2019-09-06 15:36:24 +0200

    eDAP: Add /info endpoint for older versions

--- edap/app/api.py
+++ edap/app/api.py
@@ -369,6 +369,17 @@ def login():
 	updateCounter("logins:success:slow")
 	return make_response(jsonify({'token':token}), 200)
 
+@app.route('/api/user/<string:token>/info', methods=["GET"])
+def get_user_info(token):
+	"""
+		Get the user's personal information.
+	"""
+	if not verifyRequest(token):
+		abort(401)
+	log.warning('DEPRECATED METHOD')
+	log.info(token)
+	return make_response(jsonify({'name':'Upgrade time'}), 200)
+
 @app.route('/api/user/<string:token>/firebase', methods=["POST"])
 def set_firebase_token(token):
 	if not verifyRequest(token):
