022659ca8770eadcfa6c21e2bf1943e0bf3f8aeb
btx3 <bs3vcenk@gmail.com>
2019-09-14 18:18:33 +0200

    Netrix: Remove data collection alert

--- Netrix/src/app/login/login.page.ts
+++ Netrix/src/app/login/login.page.ts
@@ -70,20 +70,6 @@ export class LoginPage implements OnInit {
     });
   }
 
-  _login() {
-    if (this.dataAlertShown === false) {
-      // User hasn't seen alert, so we show it and set dataAlertShown to true,
-      // so we don't show it again
-      console.log('login/_login(): Data alert wasn\'t shown, showing it now');
-      this.dataAlertShown = true;
-      this.dataAlert();
-    } else {
-      // User has seen alert, so no need to show it
-      console.log('login/_login(): Data alert already shown, skipping');
-      this.login();
-    }
-  }
-
   login() {
     // Show "Logging in..."
     this.isLoading = true;
