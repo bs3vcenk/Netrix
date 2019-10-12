4f827c082a6dce3189dec3a6f94270050f0f8f35
btx3 <bs3vcenk@gmail.com>
2019-10-12 13:11:52 +0200

    Netrix: Revert to old ad behaviour
    This reverts commit bd323556ad294ba8e7036c21d35263a24894e495.

--- Netrix/src/app/services/admob.service.ts
+++ Netrix/src/app/services/admob.service.ts
@@ -14,15 +14,11 @@ export class AdmobService {
     private storage: Storage
   ) {
     /* Check if the user wants to see ads */
-    this.storage.get('ad-time-preference').then(res => {
+    this.storage.get('ad-preference').then(res => {
       if (res != null) {
         this.adPreference = res;
       } else {
-        this.adPreference = false;
-      }
-      if (this.adPreference) {
-        this.repeatFactor = 7;
-        console.log('AdmobService: Reduced repeat factor enabled');
+        this.adPreference = true;
       }
     });
     console.log('AdmobService: repeatFactor is ' + this.repeatFactor);
@@ -32,24 +28,32 @@ export class AdmobService {
   }
 
   showBanner() {
-    /* Show the ad banner */
-    console.log('AdmobService/showBanner(): Showing ad banner');
-    admob.banner.show({
-      id: 'ca-app-pub-3536042070948443/8284474155',
-      size: 0, // BANNER
-    });
+    if (this.adPreference) {
+      /* Show the ad banner */
+      console.log('AdmobService/showBanner(): Showing ad banner');
+      admob.banner.show({
+        id: 'ca-app-pub-3536042070948443/8284474155',
+        size: 0, // BANNER
+      });
+    } else {
+      console.log('AdmobService/showBanner(): Not showing ad because of preference');
+    }
   }
 
   showInterstitial() {
-    if (this.interstitialRequestedTimes % this.repeatFactor === 0 && this.interstitialRequestedTimes !== 0) {
-      /* Show the interstitial ad */
-      console.log('AdmobService/showInterstitial(): Showing interstitial ad');
-      admob.interstitial.load({
-        id: 'ca-app-pub-3536042070948443/9659847570'
-      }).then(() => admob.interstitial.show());
+    if (this.adPreference) {
+      if (this.interstitialRequestedTimes % this.repeatFactor === 0 && this.interstitialRequestedTimes !== 0) {
+        /* Show the interstitial ad */
+        console.log('AdmobService/showInterstitial(): Showing interstitial ad');
+        admob.interstitial.load({
+          id: 'ca-app-pub-3536042070948443/9659847570'
+        }).then(() => admob.interstitial.show());
+      } else {
+        console.log('AdmobService/showInterstitial(): Not showing to prevent ad spam');
+      }
+      this.interstitialRequestedTimes += 1;
     } else {
-      console.log('AdmobService/showInterstitial(): Not showing to prevent ad spam');
+      console.log('AdmobService/showInterstitial(): Not showing ad because of preference');
     }
-    this.interstitialRequestedTimes += 1;
   }
 }
