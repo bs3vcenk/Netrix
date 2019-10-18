7cf069007c4efaee5e6434aa36eb4dde3d8b7a85
btx3 <bs3vcenk@gmail.com>
2019-10-18 17:28:05 +0200

    eDAP: 2.10.1 (F2)

--- edap/app/edap.py
+++ edap/app/edap.py
@@ -27,7 +27,7 @@ class InvalidResponse(eDAPError):
 class ParseError(eDAPError):
 	"""Failed to parse HTML"""
 
-EDAP_VERSION = "F1"
+EDAP_VERSION = "F2"
 __version__ = EDAP_VERSION
 
 def _format_to_date(preformat_string: str, date_format: str = "%d.%m.%Y.") -> int:
