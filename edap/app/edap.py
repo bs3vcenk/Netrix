0054c68f54fe95401c465023e35fc1633f9bd9f4
btx3 <bs3vcenk@gmail.com>
2019-11-12 14:58:44 +0100

    eDAP: Add link to Netrix in UA, update FF version

--- edap/app/edap.py
+++ edap/app/edap.py
@@ -56,7 +56,7 @@ class edap:
 	             pasw: str,
 	             parser: str = "lxml",
 	             edurl: str = "https://ocjene.skole.hr",
-	             ua: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/69.0",
+	             ua: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/70.0 (https://netrix.io)",
 	             debug: bool = False,
 	             loglevel: int = 1,
 	             hidepriv: bool = True,
