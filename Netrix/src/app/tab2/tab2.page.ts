3ba3db9899954ab050ee679cec818a27acef5fd4
btx3 <bs3vcenk@gmail.com>
2020-01-06 13:23:05 +0100

    Netrix: Comment and improve code for tab2

--- Netrix/src/app/tab2/tab2.page.ts
+++ Netrix/src/app/tab2/tab2.page.ts
@@ -20,8 +20,8 @@ export class Tab2Page {
   tests = null;
   showAllPreference = false;
   currentTests = [];
-  oneWeek = 604800000; // in ms
-  oneDay = 86400000;
+  oneWeek = 7 * 24 * 60 * 60 * 1000; // ms
+  oneDay = 24 * 60 * 60 * 1000; // ms
   currentDate = Date.now();
 
   constructor(
@@ -60,28 +60,29 @@ export class Tab2Page {
   }
 
   getMonday(timestamp: number): Date {
-    /* https://stackoverflow.com/a/4156516 */
+    /* From https://stackoverflow.com/a/4156516 */
     const d = new Date(timestamp);
     const day = d.getDay();
     const diff = d.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
     return new Date(d.setDate(diff));
   }
 
-  /*calculateRemainingDays(toDate: number): string {
-    const numberOfDays = Math.ceil(((toDate * 1000) - this.currentDate) / 1000 / 60 / 60 / 24);
-    return numberOfDays === 0 ? this.translate.instant('tab2.today') : numberOfDays.toString() + 'd';
-  }*/
-
   calculateRemainingTime(toDate: number): string {
+    /* `toDate` is expected to be a standard UNIX timestamp in seconds, but JS deals
+     * with milliseconds, so we multiply it by 1000. We then subtract the current date from
+     * `toDate` to get the number of milliseconds until the date. Finally, that is converted
+     * to days. */
     const numberOfDays = Math.ceil(((toDate * 1000) - this.currentDate) / 1000 / 60 / 60 / 24);
+    /* Convert the days to months, then round up to the lowest value (floor) since we'll be
+     * showing the exact number of days until the date next to the month. */
     const months = Math.floor(numberOfDays / 30);
-    let formattedString = '';
-    if (months >= 1) {
+    let formattedString = ''; // We'll update and return this variable
+    if (months >= 1) { // If there is at least a month until the date, include it in the string and return just that
       formattedString = months.toString() + 'm ' + (numberOfDays - (months * 30)).toString() + 'd';
-    } else {
-      if (numberOfDays === 0) {
+    } else { // If there isn't at least a month left, we can handle 2 cases
+      if (numberOfDays === 0) { // The date is today; in that case return the translated string for 'today'
         formattedString = this.translate.instant('tab2.today');
-      } else {
+      } else { // There are some days left, but less than a month; just return the number of days
         formattedString = numberOfDays + 'd';
       }
     }
