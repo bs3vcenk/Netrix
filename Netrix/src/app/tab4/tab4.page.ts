56ae1e75d523717d0d84277bd6caa52479e06fcf
btx3 <bs3vcenk@gmail.com>
2020-06-06 14:00:47 +0200

    Netrix: Implement tests view in tab4

--- Netrix/src/app/tab4/tab4.page.ts
+++ Netrix/src/app/tab4/tab4.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js';
 import 'chartjs-plugin-datalabels';
 import { ApiService } from '../services/api.service';
+import { TranslateService } from '@ngx-translate/core';
 
 /* School year starts in September */
 const SCHOOL_MONTH_ORDER = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -13,6 +14,17 @@ const SCHOOL_MONTH_ORDER = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
 })
 export class Tab4Page implements OnInit {
 
+  /* -- Exam variables -- */
+  tests = null;
+  oneWeek = 7 * 24 * 60 * 60 * 1000; // ms
+  oneDay = 24 * 60 * 60 * 1000; // ms
+  currentDate = Date.now();
+
+  dayShorthand = this.translate.instant('shorthand.day');
+  weekShorthand = this.translate.instant('shorthand.week');
+  monthShorthand = this.translate.instant('shorthand.month');
+
+  /* -- Graph variables -- */
   private averageHistoryData: Array<any>;
   private gradeHistoryData: Array<any>;
 
@@ -32,7 +44,8 @@ export class Tab4Page implements OnInit {
   recentTests: Array<any>;
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit() {
@@ -42,6 +55,11 @@ export class Tab4Page implements OnInit {
         this.formatGraphResponse();
         this.formatGradeCountResponse();
         this.setupAverage();
+      });
+      this.api.loadingFinishedTests.subscribe((val) => {
+        if (val) {
+          this.initTests();
+        }
       });
       this.createGradeHistoryChart();
       this.createGradeAmountChart();
@@ -211,6 +229,65 @@ export class Tab4Page implements OnInit {
         }
       }
     });
+  }
+
+  /* -- Functions for exams -- */
+
+  initTests() {
+    this.tests = this.api.currentTests;
+  }
+
+  convertToReadableDate(unixTimestamp: number): string {
+    const date = new Date(unixTimestamp * 1000);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return day + '.' + month + '.' + year + '.';
+  }
+
+  convertToReadableWeekSpan(startingWeekTimestamp: number): string {
+    const startUNIXStamp = this.getMonday(startingWeekTimestamp * this.oneWeek).getTime();
+    const startWeek = new Date(startUNIXStamp + this.oneWeek);
+    const startWeekMonth = startWeek.getMonth() + 1; // Months start from 0 in JS
+    const startWeekDay = startWeek.getDate();
+    const endWeek = new Date(startWeek.getTime() + (this.oneWeek - this.oneDay));
+    const endWeekMonth = endWeek.getMonth() + 1;
+    const endWeekDay = endWeek.getDate();
+    return startWeekDay + '.' + startWeekMonth + '. - ' + endWeekDay + '.' + endWeekMonth + '.';
+  }
+
+  getMonday(timestamp: number): Date {
+    /* From https://stackoverflow.com/a/4156516 */
+    const d = new Date(timestamp);
+    const day = d.getDay();
+    const diff = d.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
+    return new Date(d.setDate(diff));
+  }
+
+  calculateRemainingTime(toDate: number): string {
+    /* `toDate` is expected to be a standard UNIX timestamp in seconds, but JS deals
+     * with milliseconds, so we multiply it by 1000. We then subtract the current date from
+     * `toDate` to get the number of milliseconds until the date. Finally, that is converted
+     * to days. */
+    const numberOfDays = Math.ceil(((toDate * 1000) - this.currentDate) / 1000 / 60 / 60 / 24);
+    /* Convert the days to weeks, then round up to the lowest value (floor) since we'll be
+     * showing the exact number of days until the date next to the month. */
+    const weeks = Math.floor(numberOfDays / 7);
+    let formattedString = ''; // We'll update and return this variable
+    if (weeks >= 1) { // If there is at least a week until the date, include it in the string and return just that
+      formattedString = weeks.toString() + this.weekShorthand;
+      const leftoverDays = numberOfDays - (weeks * 7);
+      if (leftoverDays >= 1) { // If there are not enough days to fill the week, show them too
+        formattedString += ' ' + leftoverDays.toString() + this.dayShorthand;
+      }
+    } else { // If there isn't at least a week left, we can handle 2 cases
+      if (numberOfDays === 0) { // The date is today; in that case return the translated string for 'today'
+        formattedString = this.translate.instant('tab2.today');
+      } else { // There are some days left, but less than a month; just return the number of days
+        formattedString = numberOfDays.toString() + this.dayShorthand;
+      }
+    }
+    return formattedString;
   }
 
 }
